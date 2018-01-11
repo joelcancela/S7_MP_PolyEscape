7207a3ba374a5b6f1c41ac52136478787514431e
Joel Cancela Vaz <joel.cancela-vaz@etu.unice.fr>
2018-01-11 14:36:01 +0100

    A website and a project

---
+++ polyescape_website/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name polyEscapeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the polyEscapeApp
+ */
+angular.module('polyEscapeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
