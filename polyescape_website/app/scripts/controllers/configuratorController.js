0671d2147061da244c805db4d1b1dfb5e0ead9b6
Joel Cancela Vaz <joel.cancela-vaz@etu.unice.fr>
2018-01-12 10:56:24 +0100

    Added configurator view, step adding TODO

---
+++ polyescape_website/app/scripts/controllers/configuratorController.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name polyEscapeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the polyEscapeApp
+ */
+angular.module('polyEscapeApp')
+  .controller('ConfigCtrl', ['$rootScope', '$scope', '$window', '$uibModal', 'PolyEscapeAPIService',
+    function ($rootScope, $scope, $window, $uibModal, PolyEscapeAPIService) {
+
+      $rootScope.timelimitModal = {
+        templateUrl: 'views/modals/timeLimitModal.html',
+        // backdrop: 'static',
+        keyboard: false
+      };
+
+      $rootScope.storyModal = {
+        templateUrl: 'views/modals/storyModal.html',
+        // backdrop: 'static',
+        keyboard: false
+      };
+
+
+      $rootScope.escapeGameStory = "Echappez-vous dans le temps imparti en résolvant les éngimes !";
+      $rootScope.escapeGameTimeLimit = 60;
+      $rootScope.escapeGameSteps = [];
+
+      $scope.setTimeLimit = function () {
+        $rootScope.timelimitModalInstance = $uibModal.open($rootScope.timelimitModal);//creates modalForceToggle instance
+        $rootScope.timelimitModalInstance.result.then(function (res) {//result of the modal
+          if (res) {//if the user confirms the force toggle
+            $rootScope.escapeGameTimeLimit = res;
+            console.log("New time limit " + res);
+          }
+        });
+      };
+
+      $scope.setStory = function () {
+        $rootScope.storyModalInstance = $uibModal.open($rootScope.storyModal);//creates modalForceToggle instance
+        $rootScope.storyModalInstance.result.then(function (res) {//result of the modal
+          if (res) {//if the user confirms the force toggle
+            $rootScope.escapeGameStory = res;
+            console.log("New story " + res);
+          }
+        });
+      };
+
+      $scope.playEscapeGame = function () {
+        if ($rootScope.escapeGameSteps.length === 0) {
+          $window.alert("Vous n'avez pas d'étapes pour votre escape game !");
+        }else{
+          $rootScope.configIsReady = true;
+          $window.location.hash = "#/play";
+        }
+      };
+
+    }]);
