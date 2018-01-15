2344576ad78762845337e79d95f19db5e559981e
Jeremy Lara <jeremy.lara@etu.unice.fr>
2018-01-15 22:30:47 +0100

    Multiple instances handled

--- polyescape_engine/src/polytech/teamf/api/ServiceManager.java
+++ polyescape_engine/src/polytech/teamf/api/ServiceManager.java
@@ -3,33 +3,26 @@ package polytech.teamf.api;
 import org.json.JSONObject;
 import polytech.teamf.game_engine.Runner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Service handles everything that every others services might need.
  */
 public class ServiceManager {
 
-    private static Runner runner = null;
+    public static Map<String, Runner> runnersInstances = new HashMap<>();
 
     private static JSONObject lastResult = new JSONObject();
 
     /**
-     * Get the unique runner instance.
+     * Create a new unique runner instance.
      *
      * @param config The plugins configurations.
-     * @return The unique runner instance.
      */
-    public static Runner getRunnerInstance(String config) {
-        if (runner == null) {
-            runner = new Runner(config);
-        }
-        return runner;
-    }
-
-    /**
-     * Reset the runner to null
-     */
-    public static void resetRunner() {
-        runner = null;
+    public static void createNewInstance(String uuid, String config) {
+        Runner runner = new Runner(config);
+        runnersInstances.put(uuid, runner);
     }
 
     /**
--- polyescape_engine/test/apitests/RunnerServicesTest.java
+++ polyescape_engine/test/apitests/RunnerServicesTest.java
@@ -23,13 +23,14 @@ public class RunnerServicesTest extends JerseyTest {
     private JSONArray successConfig = new JSONArray();
     private JSONArray successMultipleConfig = new JSONArray();
     private JSONObject successAnswer = new JSONObject();
+    private String runnerID;
 
     @Before
     public void setup() {
         JSONObject successPluginConfig = new JSONObject();
         successPluginConfig.put("type", "CaesarCipherPlugin");
         successPluginConfig.put("description", "Put a description here");
-        successPluginConfig.put("plain_text", "CAESAR");
+        successPluginConfig.put("plain_text", "HFJXFW");
         successPluginConfig.put("cipher_padding", "5");
         successConfig.put(successPluginConfig);
         successMultipleConfig.put(successPluginConfig);
@@ -41,17 +42,20 @@ public class RunnerServicesTest extends JerseyTest {
         successPluginConfig.put("correct_answers_csv", "correct answer");
         successMultipleConfig.put(successPluginConfig);
 
-        successAnswer.put("attempt_text", "HFJXFW");
+        successAnswer.put("attempt", "HFJXFW");
     }
 
     @After
     public void tearsdown() {
-        ServiceManager.resetRunner();
+        ServiceManager.runnersInstances.clear();
     }
 
     private void instantiateRunner(JSONArray pluginConfig) {
         Entity<String> config = Entity.entity(pluginConfig.toString(), MediaType.TEXT_PLAIN);
-        target("runners/instantiate").request().put(config);
+        Response response = target("runners/instantiate").request().put(config);
+        runnerID = response.readEntity(String.class);
+        JSONObject runnerIDInJo = new JSONObject(runnerID);
+        runnerID = runnerIDInJo.getString("id");
     }
 
     private void answerRunner(JSONObject answer) {
@@ -68,7 +72,12 @@ public class RunnerServicesTest extends JerseyTest {
     public void testRunnerInstantiateRequestSuccess() {
         Entity<String> config = Entity.entity(successConfig.toString(), MediaType.TEXT_PLAIN);
         Response response = target("runners/instantiate").request().put(config); //Here we send PUT request
-        assertEquals("Status should be 201", 201, response.getStatus());
+        assertEquals("Status should be 200", 200, response.getStatus());
+        assertEquals("runnerInstances in ServiceManager should have 1 instances", 1, ServiceManager.runnersInstances.size());
+
+        runnerID = response.readEntity(String.class);
+        JSONObject runnerIDInJo = new JSONObject(runnerID);
+        assertTrue("Should have a key id", runnerIDInJo.has("id"));
     }
 
     @Test
@@ -76,22 +85,28 @@ public class RunnerServicesTest extends JerseyTest {
         Entity<String> config = Entity.entity("", MediaType.TEXT_PLAIN);
         Response response = target("runners/instantiate").request().put(config);
         assertEquals("Status should be 400", 400, response.getStatus());
+        assertEquals("runnerInstances in ServiceManager should have 0 instances", 0, ServiceManager.runnersInstances.size());
     }
 
     @Test
     public void testRunnerAnswerRequestSuccess() {
         instantiateRunner(successConfig);
         Entity<String> answer = Entity.entity(successAnswer.toString(), MediaType.TEXT_PLAIN);
-        Response response = target("runners/answer").request().post(answer);
+        Response response = target("runners/" + runnerID + "/answer").request().post(answer);
+        String result = response.readEntity(String.class);
+        JSONObject resultInJo = new JSONObject(result);
+
         assertEquals("Status should be 200", 200, response.getStatus());
         assertEquals("MediaType should be JSON", MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+        assertTrue("Should have a key success", resultInJo.has("success"));
+        assertEquals("Should be true", "true", resultInJo.getString("success"));
     }
 
     @Test
     public void testRunnerAnswerRequestFailure() {
         instantiateRunner(successConfig);
         Entity<String> answer = Entity.entity("", MediaType.TEXT_PLAIN);
-        Response response = target("runners/answer").request().post(answer);
+        Response response = target("runners/" + runnerID + "/answer").request().post(answer);
         assertEquals("Status should be 400", 400, response.getStatus());
     }
 
@@ -99,7 +114,7 @@ public class RunnerServicesTest extends JerseyTest {
     public void testRunnerStatusRequestFinishProperty() {
         instantiateRunner(successConfig);
         answerRunner(successAnswer);
-        final String response = target("runners/status").request().get(String.class);
+        final String response = target("runners/" + runnerID + "/status").request().get(String.class);
         JSONObject responseInJO = new JSONObject(response);
         assertTrue("Should be true", responseInJO.has("status"));
         assertEquals("Status should be finish", "finish", responseInJO.getString("status"));
@@ -109,10 +124,17 @@ public class RunnerServicesTest extends JerseyTest {
     public void testRunnerStatusRequestOkProperty() {
         instantiateRunner(successMultipleConfig);
         answerRunner(successAnswer);
-        final String response = target("runners/status").request().get(String.class);
+        final String response = target("runners/" + runnerID + "/status").request().get(String.class);
         JSONObject responseInJO = new JSONObject(response);
         assertTrue("Should be true", responseInJO.has("status"));
         assertEquals("Status should be ok", "ok", responseInJO.getString("status"));
+    }
+
+    @Test
+    public void testMultipleInstances() {
+        instantiateRunner(successConfig);
+        instantiateRunner(successConfig);
+        assertEquals("Should have 2 runner instances", 2, ServiceManager.runnersInstances.size());
     }
 
 }
