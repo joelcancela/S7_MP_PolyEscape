bf6172345ecdd086c411cba0bd281a68dbcb0128
Doryan Bonifassi <doryan.bonifassi@etu.unice.fr>
2018-01-16 09:17:31 +0100

    add a new Plugin : SimplePasswordPlugin which is a simple password asked

---
+++ polyescape_engine/src/polytech/teamf/plugins/SimplePasswordPlugin.java
@@ -0,0 +1,80 @@
+package polytech.teamf.plugins;
+
+import org.json.JSONObject;
+
+
+public class SimplePasswordPlugin extends Plugin{
+
+         /**
+         * The original plain text. Is used by the validation process.
+         */
+        private String plain_text = "";
+
+        /**
+         * Default constructor
+         * Used by the API Reflection Engine
+         */
+        public SimplePasswordPlugin() {
+            this("", "");
+        }
+
+        /**
+         * Initializes the plugin
+         *
+         * @param description The plugin description
+         * @param plain_text The plain text to discover
+         */
+        public SimplePasswordPlugin(String description, String plain_text) {
+
+            super(description, "Epreuve mot de passe simple");
+
+            // ARGS
+            super.getArgs().add("plain_text");
+            // SCHEMA
+            this.schema.put("attempt", "The user attempt");
+
+            // FORM
+            this.plain_text = plain_text;
+            this.ans_format = "text";
+
+
+
+
+        }
+
+        @Override
+        public JSONObject play(JSONObject args) {
+
+            JSONObject ret = new JSONObject();
+
+            try {
+                if (this.plain_text.equals(args.getString("attempt"))) {
+                    this.isValidatedState = true;
+                    ret.put(SUCCESS, "true");
+                }
+                else {
+                    ret.put(SUCCESS, "false");
+                }
+            }
+            catch (Exception e) {
+                ret.put(SUCCESS, "false");
+            }
+
+            return ret;
+        }
+
+        public String toString() {
+            return new JSONObject()
+                    .put("name", this.getName())
+                    .put("description", this.getDescription())
+                    .put("plain_text", this.plain_text)
+                    .put("answer_format",this.getAns_format() ).toString();
+        }
+
+        @Override
+        public String getDescription(){
+            return  super.getDescription();
+        }
+    }
+
+
