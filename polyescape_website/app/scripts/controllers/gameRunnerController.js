aacca02a54d72b071b1888944e7860df9b071779
Joel Cancela Vaz <joel.cancela-vaz@etu.unice.fr>
2018-01-16 12:09:36 +0100

    Fixed countdown, fixed some visual details
    Added modals win/lose and timer callback

--- polyescape_website/app/scripts/controllers/gameRunnerController.js
+++ polyescape_website/app/scripts/controllers/gameRunnerController.js
@@ -11,11 +11,29 @@ angular.module('polyEscapeApp')
   .controller('GameRunnerCtrl', ['$rootScope', '$scope', '$window', '$uibModal', 'PolyEscapeAPIService',
     function ($rootScope, $scope, $window, $uibModal, PolyEscapeAPIService) {
 
+      $rootScope.stepTimer = $rootScope.escapeGameTimeLimit * 60;
       $rootScope.currentPluginDescription = "";
       $rootScope.currentPluginIsInput = false;
       $rootScope.currentPluginNumerOfInput = 1;
       $rootScope.correctAnswerGiven = undefined;
-      $scope.currentAnswer = {"answer": "Votre réponse"};
+      $scope.currentAnswer = {"answer": ""};
+      $rootScope.startModal = {
+        templateUrl: 'views/modals/escapeGameStartModal.html',
+        backdrop: 'static',
+        size: 'lg',
+        windowClass: 'start-modal-popup',
+        keyboard: false
+      };
+      $rootScope.loseModal = {
+        templateUrl: 'views/modals/loseModal.html',
+        backdrop: 'static',
+        keyboard: false
+      };
+      $rootScope.winModal = {
+        templateUrl: 'views/modals/winModal.html',
+        backdrop: 'static',
+        keyboard: false
+      };
 
       $scope.submitAnswer = function () {
         console.log($scope.currentAnswer.answer);
@@ -39,16 +57,32 @@ angular.module('polyEscapeApp')
               $rootScope.correctAnswerGiven = undefined;
               getNextPlugin();
             } else if (result.data.status === 'finish') {
+              $rootScope.$broadcast('timer-stop');
               //TODO
               //ITS OVER
               //Stop countdown
               //Display time elapsed
+              $rootScope.winModalInstance = $uibModal.open($rootScope.winModal);//creates modalForceToggle instance
+              $rootScope.winModalInstance.result.then(function (res) {//result of the modal
+                if (res) {//if the user confirms the force toggle
+                  $window.location.hash = "#/";
+                }
+              });
               console.log("Finished");
             }
           }, function (reason) {
             alert('Failed to send request to answer ' + reason);
           });
         }
+      };
+
+      $scope.timeElapsedCallback = function(){
+        $rootScope.loseModalInstance = $uibModal.open($rootScope.loseModal);//creates modalForceToggle instance
+        $rootScope.loseModalInstance.result.then(function (res) {//result of the modal
+          if (res) {//if the user confirms the force toggle
+            $window.location.hash = "#/";
+          }
+        });
       };
 
       function getNextPlugin() {
@@ -64,6 +98,16 @@ angular.module('polyEscapeApp')
 
       function init() {
         if ($rootScope.configIsReady) {
+          //Start modal
+          $rootScope.startModalInstance = $uibModal.open($rootScope.startModal);//creates modalForceToggle instance
+          $rootScope.startModalInstance.result.then(function (res) {//result of the modal
+            if (res) {//if the user confirms the force toggle
+              $rootScope.$broadcast('timer-start');
+              console.log("Escape game started");
+            }
+          });
+
+          //Get first plugin
           var promise = PolyEscapeAPIService.instantiateRunner($rootScope.escapeGameSteps);
           promise.then(function (result) {
             console.log(result);
