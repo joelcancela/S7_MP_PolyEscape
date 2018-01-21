f15aa4a88c17862948127e26eeec0fa13ea20790
Jeremy Lara <jeremy.lara@etu.unice.fr>
2018-01-21 16:08:23 +0100

    Added service path

---
+++ polyescape_engine/src/polytech/teamf/api/ServiceServices.java
@@ -0,0 +1,32 @@
+package polytech.teamf.api;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/service")
+public class ServiceServices {
+
+    @POST
+    @Path("/{service}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getPostService(@PathParam("service") String serviceName) { // should be another parameter here for the post
+        if (serviceName.isEmpty()) { // rather test if the service name doesn't exist
+            return Response.status(400).build();
+        } else {
+            return Response.ok().build();
+        }
+    }
+
+    @GET
+    @Path("/{service}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getGetService(@PathParam("service") String serviceName) {
+        if (serviceName.isEmpty()) { // rather test if the service name doesn't exist
+            return Response.status(400).build();
+        } else {
+            return Response.ok().build();
+        }
+    }
+
+}
