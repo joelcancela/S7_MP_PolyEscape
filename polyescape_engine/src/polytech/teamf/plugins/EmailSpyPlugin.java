1fe88eac7e6559b0b9e4cecbb2966f487f7ff54a
Joel Cancela Vaz <joel.cancela-vaz@etu.unice.fr>
2018-01-21 17:40:28 +0100

    Fixing EmailSpyPlugin

--- polyescape_engine/src/polytech/teamf/plugins/EmailSpyPlugin.java
+++ polyescape_engine/src/polytech/teamf/plugins/EmailSpyPlugin.java
@@ -8,68 +8,67 @@ import java.util.Map;
 
 public class EmailSpyPlugin extends Plugin {
 
-    /**
-     * The original plain text.
-     * Is used by the validation process.
-     */
-    private String plain_text = "";
+	/**
+	 * The original plain text.
+	 * Is used by the validation process.
+	 */
+	private String plain_text = "";
 
-    /**
-     * Initializes the plugin.
-     *
-     * @param description the plugin description
-     * @param plain_text  the plain text to discover
-     */
-    public EmailSpyPlugin(String description, String plain_text) {
+	/**
+	 * Initializes the plugin.
+	 *
+	 * @param description the plugin description
+	 * @param plain_text  the plain text to discover
+	 */
+	public EmailSpyPlugin(String description, String plain_text) {
 
-        super("Epreuve mot de passe envoyé sur un email distant",
-                description + "<br>Envoyez votre réponse à cet email : <a href=\"mailto:polyescape.olw5ew@zapiermail.com\">polyescape.olw5ew@zapiermail.com</a>"
-        );
+		super("EmailSpyPlugin", description + "<br>Envoyez votre réponse à cet email : <a href=\"mailto:polyescape.olw5ew@zapiermail.com\">polyescape.olw5ew@zapiermail.com</a>"
+		);
 
-        // MODEL
-        this.plain_text = plain_text;
-    }
+		// MODEL
+		this.plain_text = plain_text;
+	}
 
-    public EmailSpyPlugin() {
-        super("EmailSpyPlugin", "");
-        args.put("description", "String");
-        args.put("plain_text", "String");
-    }
+	public EmailSpyPlugin() {
+		super("EmailSpyPlugin", "");
+		args.put("description", "String");
+		args.put("plain_text", "String");
+	}
 
-    @Override
-    public IEvent execute(Map<String, Object> args) throws Exception {
+	@Override
+	public IEvent execute(Map<String, Object> args) throws Exception {
 
-        if (!args.containsKey("attempt")) {
-            throw new IllegalArgumentException("Bad response format");
-        }
+		if (!args.containsKey("attempt")) {
+			throw new IllegalArgumentException("Bad response format");
+		}
 
-        String response = (String) args.get("attempt");
+		String response = (String) args.get("attempt");
 
-        if (this.plain_text.equals(response)) {
-            return new GoodResponseEvent(this);
-        }
+		if (this.plain_text.equals(response)) {
+			return new GoodResponseEvent(this);
+		}
 
-        return new BadResponseEvent(this);
-    }
+		return new BadResponseEvent(this);
+	}
 
-    @Override
-    public void onBadResponseEvent() {
+	@Override
+	public void onBadResponseEvent() {
 
-    }
+	}
 
-    @Override
-    public void onGoodResponseEvent() {
+	@Override
+	public void onGoodResponseEvent() {
 
-    }
+	}
 
-    @Override
-    public void onStartEvent() {
+	@Override
+	public void onStartEvent() {
 
-    }
+	}
 
-    @Override
-    public void onEndEvent() {
+	@Override
+	public void onEndEvent() {
 
-    }
+	}
 
 }
