787c9b4454c05d9e89b782df4d67e63349324563
Joel Cancela Vaz <joel.cancela-vaz@etu.unice.fr>
2018-01-22 10:21:49 +0100

    Added some translations

--- polyescape_website/app/scripts/services/TranslatorService.js
+++ polyescape_website/app/scripts/services/TranslatorService.js
@@ -15,9 +15,12 @@ angular.module('polyEscapeApp')
         correct_answers_csv: "Réponses correctes séparées par des \",\"",
         plain_text: "Réponse",
         cipher_padding: "Longueur du décalage",
+        url: "URL",
         CaesarCipherPlugin: "Epreuve Code César",
         SimplePasswordPlugin: "Epreuve Question simple",
-        MultipleChoiceQuestionPlugin: "Epreuve QCM"
+        MultipleChoiceQuestionPlugin: "Epreuve QCM",
+        'polytech.teamf.plugins.EmailSpyPlugin': "Epreuve avec Réponse par mail",
+        'polytech.teamf.plugins.GoogleSheetsPlugin':"Epreuve avec QCM Google Sheets"
       };
 
       this.translate = function(str){
